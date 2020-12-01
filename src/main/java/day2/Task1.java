487a3a5104979859f44fb5837a9378fcc0047c67
Boris <Boris@MacBook-Pro.local>
2020-12-01 12:34:51 +0300

    Day 2 tasks done

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        int floor = s.nextInt();
+        if (floor > 0 && floor < 5) {
+            System.out.println("Малоэтажный дом");
+        } else if (floor > 4 && floor < 9) {
+            System.out.println("Среднеэтажный дом");
+        } else if (floor > 8) {
+            System.out.println("Многоэтажный дом");
+        }
+        else if (floor < 1) {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
