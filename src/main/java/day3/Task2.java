1e60626f940d263ff60cec978d1a4290a6635f35
Boris <Boris@MacBook-Pro.local>
2020-12-02 12:47:16 +0300

    day 3 tasks done

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        double a;
+        double b = 1;
+        while (b != 0) {
+            //System.out.println("Введите числа");
+            a = sc.nextDouble();
+            b = sc.nextDouble();
+            if(b == 0){
+                break;
+            }
+            System.out.println(a/b);
+        }
     }
 }
