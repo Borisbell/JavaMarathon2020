2339ef80f1409c2cd0a44fed7d53ec5854bd0519
Boris <Boris@MacBook-Pro.local>
2020-12-04 13:02:32 +0300

    day 5 tasks done

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,10 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setColor("Blue");
+        car.setModel("Firebird");
+        car.setYear(1977);
+        System.out.println(car.getColor() + " " + car.getModel() + " " + car.getYear() );
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,7 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorbike bike = new Motorbike( 1957, "Chrome", "Harley-Davidson Sportster" );
+        System.out.println(bike.getYear() + " " + bike.getColor() + " " + bike.getModel());
     }
 }
