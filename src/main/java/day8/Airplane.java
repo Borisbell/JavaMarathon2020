eb0b9772ee6a0a5406ebbd1ae3d9ea60e6579442
Boris <Boris@MacBook-Pro.local>
2020-12-07 12:14:37 +0300

    day 8 tasks done

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,42 @@
+package day8;
+
+public class Airplane {
+    private String producer;
+    private int year;
+    private int length;
+    private int weight;
+    private int fuel;
+
+    public Airplane(String producer, int year, int length, int weight){
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = 0;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public int fillUp(int addedFuel) {
+        this.fuel+=addedFuel;
+        return this.fuel;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void info(){
+        System.out.printf("Изготовитель: %s, год выпуска: %d, длина: %d, вес: %d, количество топлива в баке: %d",this.producer, this.year, this.length, this.weight, this.fuel);
+    }
+
+    public String toString(){
+        return "Изготовитель: "+this.producer+", год выпуска: "+this.year+", длина: "+this.length+", вес: "+this.weight+", количество топлива в баке: "+this.fuel;
+    }
+
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,29 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        int counter = 0;
+        String result = "0";
+        long start = System.currentTimeMillis();
+        System.out.println(start);
 
+//      Конкатенация
+//        while(counter < 20000){
+//            counter ++;
+//            result+= (" "+counter);
+//        }
+
+//      Stringbuilder
+        StringBuilder sb = new StringBuilder("0");
+        while(counter < 20000){
+            counter ++;
+            sb.append((" "+counter));
+        }
+
+        System.out.println(sb);
+
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+        System.out.println(finish);
+        System.out.println(timeConsumedMillis);
     }
 }
